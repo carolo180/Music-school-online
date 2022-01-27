5122a7ef86a5ade050079a3cddd8fcda5ba8c513
carolo180 <caponeris1@gmail.com>
2022-01-26 19:15:12 -0500

    correcciones imprevistas

--- js/piano.js
+++ js/piano.js
@@ -1,5 +1,58 @@
-/* Primera manera dejando el html limpio y creando objetos */
+/* Primera manera trayendo los datos desde el html con el atributo data */
 
+
+/*Reproduccion con el click*/
+
+const teclas = document.querySelectorAll('.tecla')
+
+
+teclas.forEach(tecla => {
+  tecla.addEventListener('click', () => playNote(tecla))
+})
+
+function playNote(tecla) {
+   
+  const noteAudio = document.getElementById(tecla.dataset.note) 
+  noteAudio.currentTime = 0
+  noteAudio.play()
+ }
+
+
+/*Reproduccion con el teclado*/
+const WHITE_KEYS = ['q', 'w', 'e', 'r', 't', 'y', 'u','i', 'o', 'p', 'a', 's', 'd', 'f', ]
+const BLACK_KEYS = ['g', 'h', 'j', 'k', 'l', 'ñ', 'z', 'x', 'c', 'v']
+
+const whiteKeys = document.querySelectorAll('.tecla.white')
+const blackKeys = document.querySelectorAll('.tecla.black')
+
+
+document.addEventListener('keydown', e => {
+   if (e.repeat) return
+   const key = e.key
+   const whiteKeyIndex = WHITE_KEYS.indexOf(key)
+   const blackKeyIndex = BLACK_KEYS.indexOf(key)
+ 
+   if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex])
+   if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex])
+
+  
+ })
+
+/*Evento que reproduce el tutorial de youtube*/
+ const videos = document.getElementById("video")
+ const video = document.getElementById("you_tube")
+ const piano = document.getElementById("teclas")
+
+ videos.addEventListener('click', e =>{
+  e.preventDefault()
+  
+ piano.style.display="none"
+ video.style.display="flex"
+ video.style.visibility="visible"
+})
+
+/* Segunda manera dejando el html limpio y creando objetos esta enlazado con el archivo piano2.html */
+ 
 /*
 const c3 = new Audio("sounds/c3.mp3");
 const Db3 = new Audio("sounds/Db3.mp3");
@@ -155,57 +208,6 @@ const teclas=[
     })
   });
   */
-
-const teclas = document.querySelectorAll('.tecla')
-
-
-teclas.forEach(tecla => {
-  tecla.addEventListener('click', () => playNote(tecla))
-})
-
-
-function playNote(tecla) {
-   
-  const noteAudio = document.getElementById(tecla.dataset.note) 
-  noteAudio.currentTime = 0
-  noteAudio.play()
-  tecla.classList.add('active')
-  noteAudio.addEventListener('ended', () => {
-  tecla.classList.remove('active')
-  })
-}
-
-
-const WHITE_KEYS = ['q', 'w', 'e', 'r', 't', 'y', 'u','i', 'o', 'p', 'a', 's', 'd', 'f', ]
-const BLACK_KEYS = ['g', 'h', 'j', 'k', 'l', 'ñ', 'z', 'x', 'c', 'v']
-
-const whiteKeys = document.querySelectorAll('.tecla.white')
-const blackKeys = document.querySelectorAll('.tecla.black')
-
-
-document.addEventListener('keydown', e => {
-   if (e.repeat) return
-   const key = e.key
-   const whiteKeyIndex = WHITE_KEYS.indexOf(key)
-   const blackKeyIndex = BLACK_KEYS.indexOf(key)
- 
-   if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex])
-   if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex])
- })
-
-/*Evento que reproduce el tutorial de youtube*/
- const videos = document.getElementById("video")
- const video = document.getElementById("you_tube")
- const piano = document.getElementById("teclas")
-
- videos.addEventListener('click', e =>{
-  e.preventDefault()
-  
- piano.style.display="none"
- video.style.display="flex"
- video.style.visibility="visible"
-})
- 
  
 
   
